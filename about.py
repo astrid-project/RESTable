741a8f4141b88d5afcd29c29dcd7446133df8c10
Alex Carrega <contact@alexcarrega.com>
2021-08-11 18:48:12 +0200

    Minor changes

--- about.py
+++ about.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Alex Carrega <contact@alexcarrega.com>
+# Copyright (c) 2020-2029 Alex Carrega <contact@alexcarrega.com>
 # author: Alex Carrega <contact@alexcarrega.com>
 
 version = '0.0.1'
--- dev/run.sh
+++ dev/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Copyright (c) 2020-2029 Alex Carrega <contact@alexcarrega.com>
+# author: Alex Carrega <contact@alexcarrega.com>
+
 PORT=$(cat settings.yaml | shyaml get-value port)
 uvicorn main:app --reload --host 0.0.0.0 --port $PORT
