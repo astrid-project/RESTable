a6949bd97ff706aa0fe68e3cb837a45ac87b7e32
Alex Carrega <contact@alexcarrega.com>
2021-10-02 12:00:10 +0200

    Dev utils

--- dev/run.sh
+++ dev/run.sh
@@ -3,5 +3,4 @@
 # Copyright (c) 2020-2029 Alex Carrega <contact@alexcarrega.com>
 # author: Alex Carrega <contact@alexcarrega.com>
 
-PORT=$(cat settings.yaml | shyaml get-value port)
-uvicorn main:app --reload --host 0.0.0.0 --port $PORT
+vprof -c cmh src/main.py --output-file dev/profile.json
