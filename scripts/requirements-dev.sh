526dd9700b5d47ac8a722dc5c71c0ca6dd7d7903
Alex Carrega <contact@alexcarrega.com>
2022-01-28 09:44:07 +0100

    Minor changes

--- scripts/requirements-dev.sh
+++ scripts/requirements-dev.sh
@@ -3,4 +3,4 @@
 # Copyright (c) 2020-2029 GUARD Project <guard-project.eu>
 # author: Alex Carrega <alessandro.carrega@cnit.it>
 
-pipenv lock -r --dev-only > requirements-dev.txt
+pipenv lock -r --dev-only > dev/requirements.txt
